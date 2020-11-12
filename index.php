acc2647fd9fe101972b34953a3d256c40950d4a5
Dharani <51089668+Dharani2501@users.noreply.github.com>
2020-11-12 18:50:10 +0530

    Payment Gateway Integration files

---
+++ index.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="ISO-8859-1">
+<link rel="shortcut icon" href="assets/images/logo/logo-dark.png">
+<title>Payment Gateway Integration</title>
+<link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+<link rel="stylesheet" href="assets/vendors/menu/src/main.menu.css">
+<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+<link rel="stylesheet" type="text/css" href="assets/css/utilities.css">
+<link rel="stylesheet" type="text/css" href="assets/css/colors.css">
+<link rel="stylesheet" type="text/css" href="assets/css/colors-gh.css">
+<link rel="stylesheet" type="text/css" href="assets/css/colors-g.css">
+<!-- CAROUSEL -->
+<link rel="stylesheet" href="assets/vendors/owl.carousel/owl.carousel.min.css">
+<link rel="stylesheet" href="assets/vendors/owl.carousel/owl.theme.default.min.css">
+
+	<!-- ANIMATE -->
+<link rel="stylesheet" type="text/css" href="assets/vendors/animate/animate.css">
+
+	<!-- ANIMATED HEADLINES -->
+<link rel="stylesheet" type="text/css" href="assets/vendors/animated-headline/css/style.css">
+
+	<!-- FANCY BOX -->
+<link rel="stylesheet" type="text/css" href="assets/vendors/fancybox/jquery.fancybox.min.css">
+
+</head>
+<body class="indexPage">
+<div>
+	<div class="main-header header-shadow cherry-header">
+	    <div class="bg-cherry">
+	        <div class="container">
+	            <div class="row">
+	                <div class="col-lg-6 col-md-12">
+	                    <div class="text-white mt-10 text-center--md text-center--sm text-center--xs">
+	                        Helpline 7382653664
+	                    </div>
+	                </div>
+                  
+	            </div>
+	        </div>
+	    </div>
+	
+
+  </head>
+<body class="Total">
+<body class="indexPage">
+  
+
+<center><h1>Payment Gateway Integration</h1></center>
+<section class="pt-s5 pb-s5 overlay-wrap" data-plugin-parallax data-plugin-options="{'speed': 1.5}" data-image-src="assets/images/bg/bg-1.jpg">
+		<div class="container overlay-container pt-s2 pb-s5">
+			<div class="pb-s5">
+				<div class="hero-section-container-tl w-100 h-100 d-flex justify-content-center align-items-center">
+					<div class="text-center mb-s2">
+						<div class="mt-s3 text-white">
+							<h6>Donate for a cause</h6>
+						</div>
+						<div class="mt-s1 mb-s5 text-white">
+							<h1 class="animated-headline letters type mb-s2 text-white text-size-20--xs text-size-30--sm">
+								<span class="headline-wrapper text-cherry text-capitalize">
+								    <b>Never think of your contribution as small</b>
+									<b class="is-visible">Play your role in the constructive cause</b>
+									<b>Donating is the smart way to play your part in change</b>
+									<b>Your donation is someone’s problem solution</b>
+								</span>
+							</h1>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div> <!-- /CONTAINER -->
+	  <div class="overlay bg-v9-dark"></div>
+	</section> <!-- /SECTION -->
+  <html>
+<br>
+<body>
+    <center>
+    
+        <a href="https://test.instamojo.com/@dharanimadireddy8" rel="im-checkout" data-text="Donate" data-css-style="color:#ffffff; background:#000000; width:180px; border-radius:4px" data-image="bg5.jpg" data-layout="vertical"></a>
+        <script src="https://js.instamojo.com/v1/button.js"></script>
+    </center>
+</body>
+
+<section class="bg-dark-g pt-s5 pb-s5">
+		<div class="container">
+			<div class="mb-80 text-center">
+				<h6 class="text-bold-700 text-cherry">Payment Gateway</h6>
+				<h2 class="text-bold-700 text-white">Payment done in 3 simple steps</h2>
+				<div class="mb-s3">
+					<hr class="mb-0 bc-v1-dark">
+					<hr class="w--60 bs-solid bc-cherry bw-s5 bt-0 bl-0 br-0 mt-0">
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-4 col-md-12 col-sm-12">
+					<div class="mt-s2 mb-s2 mb-60--md mb-60--sm mb-60--xs box-shadow-parent--hover mt--parent--hover">
+						<div class="bg-white text-center pt-s4 bs-solid bc-light bw-s1 position-relative">
+							<div class="hero-section-container-tl w-100 mt--s4 mt--s5-child">
+								<img class="w--100 rounded-circle bs-solid bc-white bw-s5 box-shadow-v2-s3-child--hover" src="assets/images/logo/bank.png" height="80px" width="80px" alt="Departure & Arrival Details">
+							</div>
+							<div class="p-s2 mt-s2 mb-s2">
+								<span class="text-cherry text-bold-600">Step 1</span>
+								<h5 class="mt-s1 mb-0 text-bold-600">Enter your details</h5>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-12 col-sm-12">
+					<div class="mt-s2 mb-s2 mb-60--md mb-60--sm mb-60--xs box-shadow-parent--hover mt--parent--hover">
+						<div class="bg-white text-center pt-s4 bs-solid bc-light bw-s1 position-relative">
+							<div class="hero-section-container-tl w-100 mt--s4 mt--s5-child">
+								<img class="w--100 rounded-circle bs-solid bc-white bw-s5 box-shadow-v2-s3-child--hover" src="assets/images/steps/step-2.png" alt="Your & Vehicle Detail">
+							</div>
+							<div class="p-s2 mt-s2 mb-s2">
+								<span class="text-cherry text-bold-600">Step 2</span>
+								<h5 class="mt-s1 mb-0 text-bold-600">Select the mode of transfer</h5>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-12 col-sm-12">
+					<div class="mt-s2 mb-s2 mb-60--md mb-60--sm mb-60--xs box-shadow-parent--hover mt--parent--hover">
+						<div class="bg-white text-center pt-s4 bs-solid bc-light bw-s1 position-relative">
+							<div class="hero-section-container-tl w-100 mt--s4 mt--s5-child">
+								<img class="w--100 rounded-circle bs-solid bc-white bw-s5 box-shadow-v2-s3-child--hover" src="assets/images/steps/step-3.png" alt="Payment Detail">
+							</div>
+							<div class="p-s2 mt-s2 mb-s2">
+								<span class="text-cherry text-bold-600">Step 3</span>
+								<h5 class="mt-s1 mb-0 text-bold-600">Collect payment receipt</h5>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div> <!-- /ROW -->
+		</div> <!-- /CONTAINER -->
+		
+	</section> <!-- /SECTION -->
+	</div>
+</html>
+  
+  </div>
+  <!-- <div class="animation-area">
+    <ul class="box-area">
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+  </div> -->
+  
+  
+
+
+  <script src="assets/js/jquery-3.3.1.min.js"></script>
+	<script src="assets/vendors/appear/jquery.appear.min.js"></script>
+	<script src="assets/vendors/jquery.easing/jquery.easing.min.js"></script>
+	<script src="assets/js/popper.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/vendors/common/common.min.js"></script>
+
+	<!-- FANCY-BOX -->
+	<script src="assets/vendors/fancybox/jquery.fancybox.min.js"></script>
+
+	<!-- MENU -->
+	<script src="assets/vendors/menu/src/main.menu.js"></script>
+
+	<!-- CAROUSEL -->
+	<script src="assets/vendors/owl.carousel/owl.carousel.min.js"></script>
+
+	<!-- ANIMATED-HEADLINES -->
+	<script src="assets/vendors/animated-headline/js/animated-headline.js"></script>
+
+	<!-- THEME-CUSTOM -->
+	<script src="assets/js/main.js"></script>
+
+	<!-- THEME-INITIALIZATION-FILES -->
+	<script src="assets/js/theme.init.js"></script>
+	<script src="assets/js/custom.js"></script>
+	  
+</body>
+</html>
